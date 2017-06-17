4228aaab3bdd2d2b6ab426b8d080b26d31864e5f
Alex Gorelikov <gorelikov.alex@gmail.com>
2017-06-17 15:58:46 +0300

    Initial commit

---
+++ app/redux-example.jsx
@@ -0,0 +1,40 @@
+var redux = require('redux');
+
+console.log('Starting redux example');
+
+//function add(a, b) {
+//	return a + b;
+//}
+//
+//var a = 3;
+//function add1(b) {
+//	return a + b;
+//}
+//
+//var result;
+//function add2(a, b) {
+//	result = a + b;
+//	return result;
+//}
+//
+//function add3(a, b) {
+//	return a + b + new Date().getSeconds();
+//}
+//
+//function changeProps(obj) {
+//	return {
+//		...obj,
+//		name: 'Do'
+//	};
+////	obj.name = 'Al';
+////	return obj;
+//}
+//
+//var startingValue = {
+//	name: 'Alex',
+//	age: 34
+//};
+//
+//var res = changeProps(startingValue);
+//console.log(startingValue);
+//console.log(res);
